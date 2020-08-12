c9a84a95a2d9c10afdbba0dabf8a5d26650863c6
Sergio Sanchez <sergio.sanchezs@ingenieros.com>
2020-08-11 20:45:40 -0400

    setting up webpack with babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,12 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      exclude: '/node_modules/',
+      loader: 'babel-loader',
+    }]
   }
 }
